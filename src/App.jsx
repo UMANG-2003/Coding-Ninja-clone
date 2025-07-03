fcd3e43f5e765ee3df4aebdad33e702209a231a4
UMANG-2003 <umangkumar20003@gmail.com>
2025-07-03 23:05:36 +0530

    feat: initialize project with Vite, React, and Tailwind CSS setup
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App component with a Navbar and basic styling.
    - Added React logo SVG in the assets directory.
    - Implemented Navbar component with basic structure.
    - Configured Tailwind CSS in App.css and index.css.
    - Set up main entry point with React's StrictMode.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './component/Navbar.jsx'
+function App() {
+  return (
+    <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-white h-16 w-full'>Navbar</div>
+  )
+}
+
+export default Navbar
